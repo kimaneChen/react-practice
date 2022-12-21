b91e9940408b36f15e66c80e700e8009edd5216c
Kimane <chenjx1024@outlook.com>
2022-12-21 16:04:14 +1000

    fix/format lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,32 @@
 module.exports = {
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended"
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: ["react", "@typescript-eslint", "jest", "import"],
+  plugins: ['react', '@typescript-eslint', 'jest', 'import', 'prettier'],
   env: {
     browser: true,
     es6: true,
-    jest: true
+    jest: true,
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
-    project: "./tsconfig.json"
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   rules: {
-    "linebreak-style": "off",
-    "comma-dangle": ["warn", never],
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto"
-      }
-    ]
-  }
+    'prettier/prettier': ['error'],
+  },
 };
