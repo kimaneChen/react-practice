080b110794c660f24b4c8357f30fb39522f2fb97
Kimane <chenjx1024@outlook.com>
2022-12-22 13:31:27 +1000

    fix/build the test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /This is react practice. Test the eslint, test the PR/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
