0ad2f1c704df4697c8742b5355bce4f94b2618ab
Kimane <chenjx1024@outlook.com>
2022-12-30 21:25:22 +1000

    feats/build the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express, {Application, Request, Response} from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import gameRoute from './routes/gameRoute';
+
 const app: Application =express();
 
 app.use(express.json());
@@ -9,8 +11,6 @@ app.use(express.json());
 // here we will import routes
 
 // here we will declare the routes paths
-app.get('/',(req:Request, res:Response)=>{
-	res.send({message:'It works'});
-});
+app.use('/api/games', gameRoute);
 
 export {app};
