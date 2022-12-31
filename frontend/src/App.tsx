692ba3ae619fa10e4926be0065234da11876fc85
Kimane <chenjx1024@outlook.com>
2022-12-31 22:52:54 +1000

    add the actions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Form from './components/Form';
@@ -6,7 +7,9 @@ import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-      <Form />
+      <BrowserRouter>
+        <Route path="/" element={<Form />} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/features/games/gameSlice.ts
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Game } from '../../interfaces/Game';
+
+interface GameState {
+  games: Game[] | null;
+  loading: boolean;
+  singleGame: Game | null;
+}
+
+const initialState: GameState = {
+  games: [],
+  singleGame: null,
+  loading: false,
+};
+
+//actions are processes that get data from backend
+export const getGames = createAsyncThunk<Game[]>(
+  'games/getGames',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/games');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+//reducers -> reduce to a specific state -> changes state
+export const gameSlice = createSlice({
+  name: 'game', 
+  initialState,
+  reducers：{ 
+    setGame: ( state, action: PayloadAaction(Game[]))=>{
+      state.games= action.payload.games;
+    }
+  }，
+})
